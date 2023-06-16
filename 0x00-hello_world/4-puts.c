46edf8eb4776eed701372b9465fcbff43535b4f3
Chiamablessing <chiamablessing@gmail.com>
2023-06-16 07:41:35 -0700

    Updating puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - A program that prints a specific message using the puts function
+* Return: 0 (indicating successful execution)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
