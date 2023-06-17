ec6ed02c5a005461510e7098aafa3415ed4b23d5
Chiamablessing <chiamablessing@gmail.com>
2023-06-17 05:21:15 -0700

    Print single digit 2

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print number 0 to 9
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+        int i;
+
+        for (i = 0; i < 10; i++)
+        {
+                putchar(i + '0');
+        }
+                putchar('\n');
+
+        return (0);
+}
