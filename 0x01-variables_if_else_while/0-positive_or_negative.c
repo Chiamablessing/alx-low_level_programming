501c52bc26b70b50498fd2905ff95d5a47f33d89
Chiamablessing <chiamablessing@gmail.com>
2023-06-16 21:53:49 -0700

    Updating negative and positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,17 +4,26 @@
 /*A program to Check if an int is positive or negative */
 
 /**
- *main - Entry point
- *Return: 0 (Indicating Success)
- */
+*main - Entry point
+*Return: 0 (Indicating Success)
+*/
 int main(void)
 {
-	int n;
+int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is negative\n", n);
+}
+else
+{
+printf("%d is zero\n", n);
+}
+return (0);
+}
