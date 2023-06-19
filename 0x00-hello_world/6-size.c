f96ca5f8c6b8307b1b3e0ce9a6ab12bc94ac7fd3
Chiamablessing <chiamablessing@gmail.com>
2023-06-19 02:56:36 -0700

    Updating

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,21 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
-* main - A program that prints the size of various computer types
-* Return: 0 (indicating successful execution)
-*/
+ *main - a program that print two computer types
+ *Return: returns 0 (success)
+ */
 int main(void)
 {
-char a;
-int b;
-long c;
-long long int d;
-float e;
-printf("Size of char: %lu byte(s)\n",(unsigned long) sizeof(a));
-printf("Size of int: %lu byte(s)\n",(unsigned long) sizeof(b));
-printf("Size of long: %lu byte(s)\n",(unsigned long) sizeof(c));
-printf("Size of long long int: %lu byte(s)\n",(unsigned long) sizeof(d));
-printf("Size of float: %lu byte(s)\n",(unsigned long) sizeof(e));
-return (0);
+	char var_char;
+	int var_int;
+	long int var_lint;
+	long long int var_llint;
+	float var_float;
+
+	printf("Size of a char: %ld byte(s)\n", sizeof(var_char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(var_int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(var_lint));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(var_llint));
+	printf("Size of a float: %ld byte(s)\n", sizeof(var_float));
+
+	return (0);
 }
