91ae57cc536700907d490f23b5f7c6628531022f
Chiamablessing <chiamablessing@gmail.com>
2023-06-19 01:06:58 -0700

    Updating intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<unixstd.h>
+/**
+ *main - entry path for c programs that use exactly
+ *Return: returns 1 if successful
+ */
+
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
