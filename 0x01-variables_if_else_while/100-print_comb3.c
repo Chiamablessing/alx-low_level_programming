bbfe434ce522c545ad3e1f293cce616e36f0f5ed
Chiamablessing <chiamablessing@gmail.com>
2023-06-18 18:57:15 -0700

    Updating 100 print

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,15 +10,15 @@ int main(void)
 	int a;
 	int b;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a < 9; a++)
 	{
-		for (b = 1; b <= 10; b++)
+		for (b = 1; b <= 9; b++)
 		{
 			if (b > a)
 			{
 				putchar('0' + a);
 				putchar('0' + b);
-				if (a != 9 || b != 10)
+				if (a != 8 || b != 9)
 				{
 				}
 				else
