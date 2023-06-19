f4a47ed07dbca5528f582b9631cca2695fea366b
Chiamablessing <chiamablessing@gmail.com>
2023-06-19 01:21:27 -0700

    Updating 100 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 /**
  *main - entry path for c programs that use exactly
- *Return: returns 1 if successful
+ *Return: returns 0 if successful
  */
 
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 	return (1);
 }
