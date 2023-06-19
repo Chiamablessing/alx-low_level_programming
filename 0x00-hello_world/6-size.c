926de25dc0e2afaa08211eab9261db198e334994
Chiamablessing <chiamablessing@gmail.com>
2023-06-19 00:39:59 -0700

    Updating

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int b;
 long c;
 long long int d;
 float e;
-printf("Size of char: %lu byte(s)\n", sizeof(char));
-printf("Size of int: %lu byte(s)\n", sizeof(int));
-printf("Size of long: %lu byte(s)\n", sizeof(long));
-printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
-printf("Size of float: %lu byte(s)\n", sizeof(float));
+printf("Size of char: %lu byte(s)\n", sizeof(a));
+printf("Size of int: %lu byte(s)\n", sizeof(b));
+printf("Size of long: %lu byte(s)\n", sizeof(c));
+printf("Size of long long int: %lu byte(s)\n", sizeof(d));
+printf("Size of float: %lu byte(s)\n", sizeof(e));
 return (0);
 }
