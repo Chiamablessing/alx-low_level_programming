4d8362dde37c35aeb3b21c510de04910544bb820
Chiamablessing <chiamablessing@gmail.com>
2023-06-20 19:18:22 -0700

    Checks for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ *_isalpha - checks the alphabet character c
+ *Return: 1 if an alphabet and 0 otherwise
+ *@c: is a parameter of the function
+*/
+
+int _isalpha(int c)
+{
+	if (islower(c) || isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
