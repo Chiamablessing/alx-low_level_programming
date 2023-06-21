1f71d5032ef5fb9c3ee4c5aa30be196e961cec15
Chiamablessing <chiamablessing@gmail.com>
2023-06-20 17:26:52 -0700

    Print alphabet 100

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *main - print 10 times the alphabet
+ *Return: always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int mul;
+
+	for (mul = 0; mul < 10; mul++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
+}
