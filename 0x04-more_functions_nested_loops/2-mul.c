43ab885db243a9b4aad7466297d68c93472f36c7
Chiamablessing <chiamablessing@gmail.com>
2023-06-22 21:50:12 -0700

    Updating multi

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*mul - multiplies two int
+*@a: variable type of int
+*@b: variable type of int
+*Return: returns the prouct
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
