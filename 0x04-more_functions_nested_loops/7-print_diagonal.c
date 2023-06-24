205490e401b021e09479c9d4dbc8ebbb3e783c69
chiamablessing <chiamablessing@gmail.com>
2023-06-24 12:36:15 +0100

    Updating print diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,23 +1,24 @@
-#include "main.h"    
+#include "main.h"
 /**
-*print_diagonal - prints diagonal line
-*@n: variable type of int
-*/
-       
+ *print_diagonal - prints diagonal line
+ *@n: variable type of int
+ */
+
 void print_diagonal(int n)
 {
-     		int i = 0, b;
-       
-     		while (i < n && n > 0)
-    		{
-    			for (b = 0; b < i; b++)
-    			{
-    				_putchar(' ');
-    			}
-    			_putchar('\\');
-    			_putchar('\n');
-     			i++;
-   		}
-   		if (i == 0)
-    			_putchar('\n');
- }
+	int i = 0, b;
+
+	while (i < n && n > 0)
+	{
+		for (b = 0; b < i; b++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+		i++;
+	}
+	if (i == 0)
+
+		_putchar('\n');
+}
