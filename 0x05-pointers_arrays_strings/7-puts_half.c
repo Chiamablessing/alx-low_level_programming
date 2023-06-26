43e295e5cc202365c740b73f389207cbe2012b45
chiamablessing <chiamablessing@gmail.com>
2023-06-27 00:01:09 +0100

    Printing half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <string.h>
+/**
+ *puts_half -prints half
+ *@str: pointer parameter variable of typ
+ e int
+ */
+void puts_half(char *str)
+{
+	int i, end, half;
+
+	 end = (strlen(str));
+	 half = (end / 2);
+	for (i = 0; i < end; i++)
+	{
+		if ((half % 2) == 0)
+		{
+			if (i > half)
+			{
+				_putchar(str[i]);
+			}
+		}
+		else
+		{
+			if (i >= half)
+			{
+				_putchar(str[i]);
+			}
+		}
+	}
+	_putchar('\n');
+}
