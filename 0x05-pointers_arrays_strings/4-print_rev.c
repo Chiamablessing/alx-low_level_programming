9bdfc8e93fc50dc019f4436480a158e0ff9671b5
chiamablessing <chiamablessing@gmail.com>
2023-06-26 22:30:05 +0100

    Print in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - a string in reverse
+ * @s: pointer variable of type int
+ *    
+ */
+
+void print_rev(char *s)
+{
+	int end = (strlen(s) - 1);
+
+	while (end >= 0)
+	{
+		_putchar(s[end]);
+		end--;
+	}
+	_putchar('\n');
+}
