1fb880cdb7924a44a08d42e4d3d5838cc5dc19be
chiamablessing <chiamablessing@gmail.com>
2023-06-29 15:11:15 +0100

    Comparing strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strcmp - compare concatenates strings
+ * @s1: pointer variable parameter of type char
+ * @s2: pointer variable parameter of type char
+ * Return: comparison result
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
