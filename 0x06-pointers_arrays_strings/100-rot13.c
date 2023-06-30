4cefe724a9ea7b9ca505a467bcbdc918128b5ba6
chiamablessing <chiamablessing@gmail.com>
2023-06-30 05:13:39 +0100

    Updating

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -11,19 +11,19 @@ char *rot13(char *str)
 	int b;
 	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	
+
 	for (a = 0; str[a] != '\0'; a++)
 	{
 		for (b = 0; b < 52; b++)
-		{			
+		{
 			if (str[a] == data1[b])
 			{
 				str[a] = datarot[b];
 				break;
-		
+
 			}
 		}
 	}
 
-	return (str);	
+	return (str);
 }
