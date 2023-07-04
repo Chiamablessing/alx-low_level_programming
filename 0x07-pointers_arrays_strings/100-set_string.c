d97cac3b0a21d8dd0c02f89f147cbbc4b4ab6385
chiamablessing <chiamablessing@gmail.com>
2023-07-04 11:43:39 +0100

    Updating

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -3,7 +3,7 @@
  * set_string - changes the vavlue of a schar type
  * @s: double pointer parameter variable of type char
  * @to: pointer parameter variable of type char
- * 
+ *
  */
 void set_string(char **s, char *to)
 {
