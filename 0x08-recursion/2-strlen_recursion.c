a9470596a0db2fab36374c7ff431292d5c882caa
chiamablessing <chiamablessing@gmail.com>
2023-07-05 19:46:59 +0100

    String length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: variable parameter pointer of type char
+ * Return: returns the string length
+ */
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
