929eb7bc194f6dda555d153ee5f870ef90d8629b
chiamablessing <chiamablessing@gmail.com>
2023-07-06 13:47:02 +0100

    compares two string

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,27 +1,5 @@
 #include "main.h"
 /**
- * comp- compares s strings
- * @s1: the first string
- * @s2: the second string
- * @i: index count of he string
- * Return: 0 if string are not equal and 1 otherwise
- */
-int comp(char *s1, char *s2, int i)
-{
-	if (s2[i])
-	{
-		if (s2[i] == '*')
-		{
-			s2[i] = s1[i];
-		}
-		if (s1[i] != s2[i])
-			return (0);
-		comp(s1, s2, ++i);
-	}
-	return (1);
-}
-
-/**
  * wildcmp - get the cmp function
  * @s1: the first string passed to the function
  * @s2: the second string passed to the function
@@ -31,10 +9,22 @@ int comp(char *s1, char *s2, int i)
 
 int wildcmp(char *s1, char *s2)
 {
-	int i = 0, f;
 
-	f = comp(s1, s2, i);
-	if (f == 0)
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+
+		if (*s1 == *s2)
+			return (wildcmp(s1 + 1, s2 + 1));
+
 		return (0);
-	return (1);
 }
