999b290ca61c5cc4c8dc6dff72c4c80ecfb142d0
Chiamablessing <chiamablessing@gmail.com>
2023-07-18 10:02:43 -0700

    Fixing conflict

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 /**
-<<<<<<< HEAD
  *main - prints the name of the file followed by a new line
  *
- *Return: always 0
-=======
- * main - prints the name of the file followed by a new line
- *
  * Return: always 0
->>>>>>> f6d7ea21c97fd737b7e951d2ae8fabcd92e7d63a
  */
 
 int main(void)
