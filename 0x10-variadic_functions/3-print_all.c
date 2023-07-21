14b189b71701618aeb12c232455da35282f8164f
chiamablessing <chiamablessing@gmail.com>
2023-07-21 05:59:29 +0100

    Updating

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,46 @@
+#include "variadic_functions.h"
+/**
+ * print_all - prints out output with defined format
+ * @format: list of acceptable formats
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int n = 0, i = 0;
+	char *sep = ", ";
+	char *str;
+
+	va_start(args, format);
+
+	while (format && format[i])
+		i++;
+
+	while (format && format[n])
+	{
+		if (n  == (i - 1))
+		{
+			sep = "";
+		}
+		switch (format[n])
+		{
+			case 'c':
+				printf("%c%s", va_arg(args, int), sep);
+				break;
+			case 'i':
+				printf("%d%s", va_arg(args, int), sep);
+				break;
+			case 'f':
+				printf("%f%s", va_arg(args, double), sep);
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s%s", sep, str);
+				break;
+		}
+		n++;
+	}
+	printf("\n");
+	va_end(args);
+}
