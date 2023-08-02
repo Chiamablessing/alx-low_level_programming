86a50d72d4634a0a9a1dda3b8d7e3db690898e7e
Chiamablessing <chiamablessing@gmail.com>
2023-08-02 01:15:58 +0100

    Updating 9 insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,18 +28,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	temp = (*head);
 
-	while (i < (idx - 1) && temp != NULL)
+	while (temp != NULL && i < (idx - 1))
 	{
 		temp = temp->next;
 		i++;
 	}
-	if (i == (idx - 1) && temp)
-	{
+	if (temp == NULL)
+		 return (NULL);
+
 		new->next = temp->next;
 		temp->next = new;
-		return (new);
-	}
-	else
-		free(new);
-	return (NULL);
+	
+	return (new);
 }
